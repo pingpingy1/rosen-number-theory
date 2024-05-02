78e7d3bba5c3eee8990bfc08e712aed438d4eb91
Heewon Lee <pingpingy@kaist.ac.kr>
2024-05-03 01:15:38 +0900

    Add UI and unit tests

---
+++ rosen_number_theory/lib/ch1_sec3_prob3.ml
@@ -0,0 +1,45 @@
+(* Calculating the Egyptian fraction *)
+
+type frac = Fr of int * int
+
+let num = fst
+let denom = snd
+
+let string_of_frac frac =
+  (frac |> num |> string_of_int) ^ "/" ^ (frac |> denom |> string_of_int)
+
+let print_frac frac = frac |> string_of_frac |> print_string
+
+let modulo x y =
+  let res = x mod y in
+  if res >= 0 then res else res + y
+
+let rec gcd a b = if b = 0 then a else gcd b (modulo a b)
+
+let simplify frac =
+  let div = gcd (num frac) (denom frac) in
+  (num frac / div, denom frac / div)
+
+let subtract frac1 frac2 =
+  let num1 = num frac1 in
+  let denom1 = denom frac1 in
+  let num2 = num frac2 in
+  let denom2 = denom frac2 in
+  simplify ((num1 * denom2) - (num2 * denom1), denom1 * denom2)
+
+let greedy_egypt frac =
+  let max_den = ((denom frac - 1) / num frac) + 1 in
+  let greedy_res = (1, max_den) in
+  (greedy_res, subtract frac greedy_res)
+
+let rec egyptian_frac frac acc =
+  if num frac = 1 then frac :: acc
+  else
+    let res, rem = greedy_egypt frac in
+    egyptian_frac rem (res :: acc)
+
+let print_egyptian_frac frac =
+  let () = print_frac frac in
+  let () = print_string " = " in
+  egyptian_frac frac [] |> List.map string_of_frac |> String.concat " + "
+  |> print_endline
--- rosen_number_theory/src/main.ml
+++ rosen_number_theory/src/main.ml
@@ -1,1 +1,34 @@
-let () = print_endline "Hello, World!"
+open Rosen_number_theory.Ch1_sec3_prob3
+
+let prompt_for_pos_int prompt =
+  let rec try_again () =
+    match read_int_opt () with
+    | None ->
+        let () = print_endline "Please, enter a positive integer" in
+        try_again ()
+    | Some n when n < 1 ->
+        let () = print_endline "Please, enter a positive integer" in
+        try_again ()
+    | Some n -> n
+  in
+  let () = print_endline prompt in
+  try_again ()
+
+let ch1_sec3_prob3 () =
+  let num =
+    prompt_for_pos_int "Please enter a positive integer as the numerator:"
+  in
+  let denom =
+    prompt_for_pos_int "Please enter a positive integer as the denominator:"
+  in
+  print_egyptian_frac (num, denom)
+
+let main () =
+  let chapter = prompt_for_pos_int "Please enter the chapter:" in
+  let section = prompt_for_pos_int "Please enter the section:" in
+  let problem = prompt_for_pos_int "Please enter the problem number:" in
+  match (chapter, section, problem) with
+  | 1, 3, 3 -> ch1_sec3_prob3 ()
+  | _ -> print_endline "Sorry, I didn\'t solve that problem yet."
+
+let () = main ()
